71876be8f01af6e88eec25c1b8c66028534012d9
ZZIDZZ <zidanutomo@gmail.com>
2022-03-06 21:01:07 +0700

    added praktikum 2

---
+++ Praktikum2/bujurSangkar.c
@@ -0,0 +1,17 @@
+// Nama      : Zidan Rafindra Utomo
+// NIM       : 24060121130051
+// Nama file : bujurSangkar.c
+// Deskripsi : menghitung luas bujur sangkar
+#include <stdio.h>
+
+int main(){
+// Kamus 
+    int sisi;
+    
+// Algoritma
+    // input
+    scanf("%d", &sisi);
+
+    // output dan proses
+    printf("\n%d", sisi*sisi);
+}
